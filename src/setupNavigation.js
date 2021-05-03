32de406c3086fec17d960da41b1e701dd9c1a052
Agon Karimani <agonkarimani@gmail.com>
2021-05-03 13:00:31 +0200

    Add buttons and pages

---
+++ src/setupNavigation.js
@@ -0,0 +1,17 @@
+import getAllElements from './utils/getAllElements'
+
+export default function setupNavigation() {
+  const pages = getAllElements('[data-page]')
+  const buttons = getAllElements('[data-nav]')
+
+  buttons.forEach(button => {
+    const buttonName = button.dataset.nav
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.page
+        page.classList.toggle('hidden', pageName !== buttonName)
+        button.classList.toggle('active', pageName === buttonName)
+      })
+    })
+  })
+}
